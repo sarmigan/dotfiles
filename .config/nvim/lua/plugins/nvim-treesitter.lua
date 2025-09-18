f1cc9176440e5dede61f1306ee13e464eec9501b
Sarmigan Sritharan <sarmigan@live.co.uk>
2025-09-18 16:12:53 +0100

    fix nvim-treesitter auto install parsers

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -20,5 +20,8 @@ return {
                 additional_vim_regex_highlighting = false,
             },
         },
+        config = function(_, opts)
+            require("nvim-treesitter.configs").setup(opts)
+        end
     },
 }
